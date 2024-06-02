9e0eedb9a1da2c30497195571b752f2aa292138b
harunadev <me@haruna.dev>
2024-06-03 03:02:44 +0900

    스태프 섹션 + smoothstate 수정

--- assets/script.js
+++ assets/script.js
@@ -11,21 +11,45 @@ $(function(){
 			onStart: {
 				duration: 500,
 				render: function ($container) {
-					
+					$(".background-dim").addClass("dim");
 					smoothState.restartCSSAnimations();
 				}
 			},
 			onReady: {
 				duration: 500,
 				render: function ($container, $newContent) {
-					
+					clearBG();
+					setPath();
 					$container.html($newContent);
+					$("#main").addClass(sessionStorage.getItem("bg") + "-bg");
 				}
 			},
+			onAfter: function($container, $newContent) {
+				$(".background-dim").removeClass("dim");
+			}
 		},
 		smoothState = $page.smoothState(options).data('smoothState');
 });
 
 $(window).on('load', function () {
+	$(".background-dim").removeClass("dim");
 	console.log("page ready");
 });
+
+function setPath() {
+	console.log("setpath " + window.location.pathname);
+	if (window.location.pathname.includes("/about")) {
+		sessionStorage.setItem("bg", 'about');
+	} else if (window.location.pathname.includes("/series")) {
+		sessionStorage.setItem("bg", 'series');
+	} else if (window.location.pathname.includes("/staffs")) {
+		sessionStorage.setItem("bg", 'staffs');
+	}
+	console.log("result " + sessionStorage.getItem("b"));
+}
+
+function clearBG() {
+	$("#main").removeClass("about-bg");
+	$("#main").removeClass("series-bg");
+	$("#main").removeClass("staffs-bg");
+}
