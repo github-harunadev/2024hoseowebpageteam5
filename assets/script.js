cd764876738662e78bd380862d146d9477b3c178
harunadev <me@haruna.dev>
2024-06-03 01:38:01 +0900

    smoothstate.js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,22 @@
-$(function() {
-  $('#main').smoothState();
+$(function(){
+  'use strict';
+  var options = {
+    prefetch: true,
+    cacheLength: 2,
+    onStart: {
+      duration: 250,
+      render: function ($container) {
+		  console.log("onStart");
+        smoothState.restartCSSAnimations();
+      }
+    },
+    onReady: {
+      duration: 0,
+      render: function ($container, $newContent) {
+		  console.log("onReady");
+        $container.html($newContent);
+      }
+    }
+  },
+  smoothState = $('#main').smoothState(options).data('smoothState');
 });
