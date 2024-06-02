2018b8492a029c40508776624036332a6f0d0631
harunadev <me@haruna.dev>
2024-06-03 01:47:00 +0900

    not working

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,31 @@
+console.log("script.js init");
+
 $(function(){
-  'use strict';
-  var options = {
-    prefetch: true,
-    cacheLength: 2,
-    onStart: {
-      duration: 250,
-      render: function ($container) {
-		  console.log("onStart");
-        smoothState.restartCSSAnimations();
-      }
-    },
-    onReady: {
-      duration: 0,
-      render: function ($container, $newContent) {
-		  console.log("onReady");
-        $container.html($newContent);
-      }
-    }
-  },
-  smoothState = $('#main').smoothState(options).data('smoothState');
+	'use strict';
+	var $page = $('#main'),
+		options = {
+			blacklist: '.no-smoothState',
+			debug: true,
+			prefetch: true,
+			cacheLength: 2,
+			onStart: {
+				duration: 500,
+				render: function ($container) {
+					
+					smoothState.restartCSSAnimations();
+				}
+			},
+			onReady: {
+				duration: 500,
+				render: function ($container, $newContent) {
+					
+					$container.html($newContent);
+				}
+			},
+		},
+		smoothState = $page.smoothState(options).data('smoothState');
+});
+
+$(window).on('load', function () {
+	console.log("page ready");
 });
