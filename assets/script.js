18e10a784e40d034b5c783f64c9ac1add6bb3d23
harunadev <me@haruna.dev>
2024-06-10 20:07:39 +0900

    시리즈 & 출연진 페이지

--- assets/script.js
+++ assets/script.js
@@ -55,3 +55,42 @@ function clearBG() {
 	$("#main").removeClass("series-bg");
 	$("#main").removeClass("staffs-bg");
 }
+
+function openStaffDialog(index) {
+	$(".staff-info-dialog").removeClass("hide");
+	$(".staff-info-dialog").addClass("show");
+	
+	$("#staffdata_img").attr("src", $("#staffdata" + index + "_img").attr("src"));
+	$("#staffdata_name").html($("#staffdata" + index + "_kname").html() + "<span>" + $("#staffdata" + index + "_ename").html() + "</span>");
+	$("#staffdata_birth").html($("#staffdata" + index + "_birth").html());
+	$("#staffdata_works").html($("#staffdata" + index + "_works").html());
+}
+
+function closeStaffDialog() {
+	$(".staff-info-dialog").addClass("hide");
+	$(".staff-info-dialog").removeClass("show");
+}
+
+function openSeriesDialog(index) {
+	$(".series-info-dialog").removeClass("hide");
+	$(".series-info-dialog").addClass("show");
+	
+	$("#seriesdata_img").attr("src", $("#seriesdata" + index + "_img").attr("src"));
+	$("#seriesdata_videolink").attr("onclick", "loadTrailer(" + index + ")");
+	$("#seriesdata_fname").html($("#seriesdata" + index + "_fname").html().substring($("#seriesdata" + index + "_fname").html().indexOf("</span>") + 7));
+	$("#seriesdata_desc").html($("#seriesdata" + index + "_desc").html());
+	$("#seriesdata_releasedate").html($("#seriesdata" + index + "_releasedate").html());
+	$("#seriesdata_runningtime").html($("#seriesdata" + index + "_runningtime").html());
+	$("#seriesdata_director").html($("#seriesdata" + index + "_director").html());
+	$("#seriesdata_staffs").html($("#seriesdata" + index + "_staffs").html());
+	
+}
+
+function closeSeriesDialog() {
+	$(".series-info-dialog").addClass("hide");
+	$(".series-info-dialog").removeClass("show");
+}
+
+function loadTrailer(index) {
+	window.open($("#seriesdata" + index + "_videolink").html(), '_blank').focus();
+}
